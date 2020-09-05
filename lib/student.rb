09098cb7244594f4d317a02c6cf2fb52a2f3710a
Nick Lanese <nlanese21@gmail.com>
2020-09-05 21:04:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,47 @@
+require 'pry'
+
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    #binding.pry
+    student_hash.each do | key, value |
+      if (key = :name)
+        @name = value
+      elsif (key = :location)
+        @location = value
+      elsif (key = :twitter)
+        @twitter = value
+      elsif (key = :linkedin)
+        @linkedin = value
+      elsif (key = :github)
+        @github = value
+      elsif (key = :blog)
+        @blog = value
+      elsif (key = :profile_quote)
+        @profile_quote = value
+      elsif (key = :bio)
+        @bio = bio
+      elsif (key = :profile_url)
+        @profile_quote = value
+      end
+      #binding.pry
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+
   end
 
   def self.all
-    
+
   end
 end
-
